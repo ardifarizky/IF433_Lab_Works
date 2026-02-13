a4378758f911660a02785ea6520922358029bb0d
Ardifa Rizky Saputra <liankylian24@gmail.com>
2026-02-13 23:17:02 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_133508_ArdifaRizkySaputra/week01/GameStore.kt
+++ src/oop_133508_ArdifaRizkySaputra/week01/GameStore.kt
@@ -4,3 +4,5 @@ fun main() {
     val gameTitle = "Persona 4 Golden"
     val price = 240000
 }
+
+fun calculateDiscount(price: Int): Int = if (price > 500000) (price * 0.20).toInt() else (price * 0.10).toInt()
