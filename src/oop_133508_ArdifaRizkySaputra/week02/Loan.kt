d336ba51236500cce40751a861cc4a95fb0bb317
Ardifa Rizky Saputra <liankylian24@gmail.com>
2026-02-13 22:26:36 +0700

    week02: (task 1) implement library input system

--- src/oop_133508_ArdifaRizkySaputra/week02/Loan.kt
+++ src/oop_133508_ArdifaRizkySaputra/week02/Loan.kt
@@ -1,5 +1,7 @@
 package oop_133508_ArdifaRizkySaputra.week02
 
+import java.util.Scanner
+
 class Loan (
     val bookTitle: String,   // Tambahkan val
     val borrower: String,    // Tambahkan val
@@ -14,4 +16,24 @@ class Loan (
             println("Total denda kamu adalah Rp. $denda")
         }
     }
+}
+
+fun main(){
+    val scanner = Scanner(System.`in`)
+    print("Masukkan Judul Buku")
+    val bookTitle = scanner.nextLine()
+    print("Masukkan Nama Peminjam")
+    val borrower = scanner.nextLine()
+    print("Masukkan Lama Pinjam Buku")
+    val loanDuration = scanner.nextInt()
+    if (loanDuration < 0) {
+        println("WARNING: Durasi tidak valid, diatur otomatis ke 1 hari.")
+        val loanDuration = 1
+    }
+    val pinjaman = Loan(bookTitle, borrower, loanDuration)
+    println("\n--- DETAIL PEMINJAMAN BUKU GACOR ---")
+    println("Judul Buku  : ${pinjaman.bookTitle}")
+    println("Peminjam    : ${pinjaman.borrower}")
+    println("Lama Pinjam : ${pinjaman.loanDuration} hari")
+    pinjaman.calculateFine()
 }
