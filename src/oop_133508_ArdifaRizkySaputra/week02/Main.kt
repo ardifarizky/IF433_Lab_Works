62aac76bf9e27a6734c37c763ac6692a4667a19a
Ardifa Rizky Saputra <liankylian24@gmail.com>
2026-02-13 10:45:53 +0700

    week02: implement user input and pre-validation

---
+++ src/oop_133508_ArdifaRizkySaputra/week02/Main.kt
@@ -0,0 +1,27 @@
+package oop_133508_ArdifaRizkySaputra.week02
+
+import java.util.Scanner
+
+fun Main() {
+    val scanner = Scanner(System.`in`)
+
+    println("--- APLIKASI PMB UMN ---")
+
+    print("Masukkan Nama: ")
+    val name = scanner.nextLine()
+
+    print("Masukkan NIM (Wajib 6 Karakter): ")
+    val nim = scanner.next()
+
+    scanner.nextLine()
+
+    if (nim.length != 6) {
+        println("ERROR: Pendaftaran dibatalkan. NIM harus 6 karakter!")
+    } else {
+        print("Masukkan Jurusan: ")
+        val major = scanner.nextLine()
+
+        val s1 = Student(name, nim, major)
+        println("Status: Pendaftaran Selesai.")
+    }
+}
