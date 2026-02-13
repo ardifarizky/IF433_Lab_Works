c268fd72875bd50c83d9802d87aed344a1f2cd05
Ardifa Rizky Saputra <liankylian24@gmail.com>
2026-02-13 21:34:37 +0700

    week02: challenge refactor primary constructor using default args

--- src/oop_133508_ArdifaRizkySaputra/week02/Student.kt
+++ src/oop_133508_ArdifaRizkySaputra/week02/Student.kt
@@ -3,12 +3,13 @@ package oop_133508_ArdifaRizkySaputra.week02
 class Student (
     val name: String,
     val nim: String,
-    val major: String
+    var gpa: Double = 0.0,
+    var major: String
 ){
     constructor(name: String, nim: String) : this(name, nim, "Non-Matriculated")
     {println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")}
     init {
-        if (nim.length != 5) {
+        if (nim.length != 6) {
             println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
             println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
         } else {
