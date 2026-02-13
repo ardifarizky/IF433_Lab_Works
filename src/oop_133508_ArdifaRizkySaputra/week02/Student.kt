4b8b2b16bdf3d5ec224e3a8f36363445a9f8676a
Ardifa Rizky Saputra <liankylian24@gmail.com>
2026-02-13 21:50:46 +0700

    week02: implement selection logic for constructors

--- src/oop_133508_ArdifaRizkySaputra/week02/Student.kt
+++ src/oop_133508_ArdifaRizkySaputra/week02/Student.kt
@@ -3,11 +3,13 @@ package oop_133508_ArdifaRizkySaputra.week02
 class Student (
     val name: String,
     val nim: String,
-    var gpa: Double = 0.0,
-    var major: String
+    var major: String,
+    var gpa: Double = 0.0
 ){
-    constructor(name: String, nim: String) : this(name, nim, "Non-Matriculated")
-    {println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")}
+    constructor(name: String, nim: String) : this(name, nim, "Non-Matriculated") {
+        println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")
+    }
+
     init {
         if (nim.length != 6) {
             println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
