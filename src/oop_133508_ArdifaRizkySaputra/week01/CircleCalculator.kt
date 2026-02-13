c6135ababea2fb35a4e4a8c50e5e696b7f3ae4ed
Ardifa Rizky Saputra <liankylian24@gmail.com>
2026-02-13 23:00:03 +0700

    week01: (tugas 1) clean up unused argos and comments

--- src/oop_133508_ArdifaRizkySaputra/week01/CircleCalculator.kt
+++ src/oop_133508_ArdifaRizkySaputra/week01/CircleCalculator.kt
@@ -1,14 +1,10 @@
 package oop_001_johnthor.week01
 
-fun main(args: Array<String>) {
-    // Variable definition
+fun main() {
     val radius = 7.0
     val pi = 3.14
-    // Calculation
     val area: Double = pi * radius * radius
-    // Output Concatenation
     println("Radius: $radius" + ", Area: $area")
-    // Logic check
     checkSize(area)
 }
 fun checkSize(area: Double) = if (area > 100) "This is a Big Circle" else "This is a Small Circle"
