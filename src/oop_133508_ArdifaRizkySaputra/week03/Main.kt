e0f6df587dab1ddd100da29ec724485848e29944
Ardifa Rizky Saputra <liankylian24@gmail.com>
2026-02-20 11:05:01 +0700

    week03: (task 1) implement Weapon testing in main

--- src/oop_133508_ArdifaRizkySaputra/week03/Main.kt
+++ src/oop_133508_ArdifaRizkySaputra/week03/Main.kt
@@ -1,6 +1,7 @@
 package oop_133508_ArdifaRizkySaputra.week03
 
 fun main() {
+    println("--- Practice Week 03 ---")
     val e = Employee("Budi")
 
     e.salary = -5000
@@ -10,4 +11,22 @@ fun main() {
     e.increasePerformance()
 
     println("Pajak yang harus dibayar: ${e.tax}")
+    println("")
+
+    println("--- Tugas Week 03 ---")
+    val myWeapon = Weapon("Pak Darman Legendary Sword")
+    println("Senjata Pilihan: ${myWeapon.name}")
+
+    println("Mencoba set damage ke -67")
+    myWeapon.damage = -67
+    println("Damage saat ini: ${myWeapon.damage}")
+    println("")
+
+    println("Mencoba set damage ke 9999")
+    myWeapon.damage = 9999
+    println("Damage saat ini: ${myWeapon.damage}")
+    println("")
+
+    println("Tier Senjata: ${myWeapon.tier}")
+
 }
