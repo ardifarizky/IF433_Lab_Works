a23282560cdfcc0c0ecc68d9271cd63bd0d263cf
Ardifa Rizky Saputra <liankylian24@gmail.com>
2026-02-20 11:15:32 +0700

    week03: (task 2) implement Player testing in main

--- src/oop_133508_ArdifaRizkySaputra/week03/Main.kt
+++ src/oop_133508_ArdifaRizkySaputra/week03/Main.kt
@@ -10,23 +10,30 @@ fun main() {
 
     e.increasePerformance()
 
-    println("Pajak yang harus dibayar: ${e.tax}")
-    println("")
+    println("Pajak yang harus dibayar: ${e.tax}\n")
 
-    println("--- Tugas Week 03 ---")
+    println("--- Tugas Week 03 (1)---")
     val myWeapon = Weapon("Pak Darman Legendary Sword")
     println("Senjata Pilihan: ${myWeapon.name}")
 
     println("Mencoba set damage ke -67")
     myWeapon.damage = -67
-    println("Damage saat ini: ${myWeapon.damage}")
-    println("")
+    println("Damage saat ini: ${myWeapon.damage}\n")
 
     println("Mencoba set damage ke 9999")
     myWeapon.damage = 9999
-    println("Damage saat ini: ${myWeapon.damage}")
+    println("Damage saat ini: ${myWeapon.damage}\n")
+    println("Tier Senjata: ${myWeapon.tier}\n")
+
+    println("--- Tugas Week 03 (2)---")
+    val player = Player("ArdifaGanteng123")
     println("")
-
-    println("Tier Senjata: ${myWeapon.tier}")
-
+    println("===== Status Awal =====")
+    println("User: ${player.username}, Level: ${player.level}")
+    println("\n--- Menambah 50 XP ---")
+    player.addXp(50)
+    println("\n--- Menambah 60 XP ---")
+    player.addXp(60)
+    println("\n===== Status Akhir =====")
+    println("User: ${player.username}, Level: ${player.level}")
 }
