3bfd768724bb7005b197ba16612fee8cddeeeaa0
Ardifa Rizky Saputra <liankylian24@gmail.com>
2026-03-05 23:30:01 +0700

    week04: (task 2) create parent class Employee

---
+++ src/oop_133508_ArdifaRizkySaputra/week4/Employee.kt
@@ -0,0 +1,17 @@
+package oop_133508_ArdifaRizkySaputra.week4
+
+class Employee {
+    open class Employee(
+        val name: String,
+        val baseSalary: Int
+    ) {
+
+        open fun work() {
+            println("$name sedang bekerja.")
+        }
+
+        open fun calculateBonus(): Int {
+            return (baseSalary * 0.1).toInt()
+        }
+    }
+}
