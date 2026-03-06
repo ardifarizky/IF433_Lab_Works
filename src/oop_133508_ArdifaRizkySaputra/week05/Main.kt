bf4b2ed2facddab391cab588f2ea1cfd92eaf854
Ardifa Rizky Saputra <liankylian24@gmail.com>
2026-03-06 10:45:27 +0700

    week05: implement polymorphic collection and smart casting

---
+++ src/oop_133508_ArdifaRizkySaputra/week05/Main.kt
@@ -0,0 +1,22 @@
+package oop_133508_ArdifaRizkySaputra.week05
+
+fun main() {
+    val dosen1 = Dosen("Pak Webe", "676767")
+    val admin1 = Admin("Bu Yuli")
+    val daftarPegawai: List<Pegawai> = listOf(dosen1, admin1)
+    for (pegawai in daftarPegawai) {
+        pegawai.bekerja()
+        when (pegawai) {
+            is Dosen -> {
+                println("Terdeteksi sebagai dosen (NIDN: ${pegawai.nidn})")
+                pegawai.mengajar()
+            }
+
+            is Admin -> {
+                println("Terdeteksi sebagai Admin")
+                pegawai.doAdminWork()
+            }
+        }
+        println("----")
+    }
+}
