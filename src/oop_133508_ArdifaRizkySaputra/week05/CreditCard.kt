24b7734eaac71f34f0b2e71ed4d8d7af3f1804a8
Ardifa Rizky Saputra <liankylian24@gmail.com>
2026-03-06 11:21:45 +0700

    week05: (task 2) create CreditCard class

---
+++ src/oop_133508_ArdifaRizkySaputra/week05/CreditCard.kt
@@ -0,0 +1,12 @@
+package oop_133508_ArdifaRizkySaputra.week05
+
+class CreditCard (val limit: Double, var usedAmount: Double = 0.0) : PaymentMethod("QRIS") {
+    override fun processPayment(amount: Double) {
+        if ((usedAmount + amount) < limit) {
+            usedAmount += amount
+            println("Transaksi berhasil!")
+        } else {
+            println("Kartu kredit anda sudah limit, transaksi ditolak, silahkan pinjam ke pinjol")
+        }
+    }
+}
