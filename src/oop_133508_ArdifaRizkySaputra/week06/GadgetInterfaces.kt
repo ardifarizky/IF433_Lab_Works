ed4bc5ced2abd9cb6c9d042d096869a7c52d270d
Ardifa Rizky Saputra <liankylian24@gmail.com>
2026-03-13 10:51:43 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_133508_ArdifaRizkySaputra/week06/GadgetInterfaces.kt
@@ -0,0 +1,13 @@
+package oop_133508_ArdifaRizkySaputra.week06
+
+interface camera {
+    fun turnOn() {
+        println("lensa kamera terbuka dan sensor aktif")
+    }
+}
+
+interface Phone {
+    fun turnOn() {
+        println("Sinyal seluler mencari jaringan")
+    }
+}
