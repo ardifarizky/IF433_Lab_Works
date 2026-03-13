771bcf1a7f70a66784432ab16bfd91b1b4df15f9
Ardifa Rizky Saputra <liankylian24@gmail.com>
2026-03-13 11:15:17 +0700

    week06: (task) instantiate smart devices in main

--- src/oop_133508_ArdifaRizkySaputra/week06/Main.kt
+++ src/oop_133508_ArdifaRizkySaputra/week06/Main.kt
@@ -5,7 +5,7 @@ fun processCheckout(method: PaymentMethod, amount: Double){
     method.pay(amount)
 }
 
-fun Main(){
+fun main(){
     val myWatch = Smartwatch()
     myWatch.showTime()
 
@@ -18,4 +18,15 @@ fun Main(){
     println("\nTesting Checkout")
     processCheckout(pay1, 50000.0)
     processCheckout(pay2, 150000.0)
+
+    println("\nSmart Home")
+    val mySmartHome = SmartHomeHub()
+    val lampuTamu = SmartLamp("L-001", "Ruang Tamu")
+    val googleNest = SmartSpeaker("S-002", "Google Nest Dapur")
+    val cctvGarasi = SmartCCTV("C-003", "Ezviz Garasi")
+    mySmartHome.addDevice(lampuTamu)
+    mySmartHome.addDevice(googleNest)
+    mySmartHome.addDevice(cctvGarasi)
+    mySmartHome.activateSecurityMode()
+    mySmartHome.turnOffAllSwitches()
 }
