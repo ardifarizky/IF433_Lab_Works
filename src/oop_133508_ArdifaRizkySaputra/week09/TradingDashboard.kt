caed1fce0f123acdd96693be294acfa059695622
Ardifa Rizky Saputra <liankylian24@gmail.com>
2026-04-24 10:04:04 +0700

    week09: (task) print worst performers and unique pairs

--- src/oop_133508_ArdifaRizkySaputra/week09/TradingDashboard.kt
+++ src/oop_133508_ArdifaRizkySaputra/week09/TradingDashboard.kt
@@ -29,6 +29,13 @@ fun main() {
         .toSet()
 
     println("=== CRYPTO TRADING DASHBOARD ===")
+
     println("\n--- TOP PERFORMERS (WIN) ---")
     topPerformersString.forEach { println(it) }
+
+    println("\n--- WORST PERFORMERS (LOSS) ---")
+    worstPerformersString.forEach { println(it) }
+
+    println("\n--- UNIQUE TRADING PAIRS ---")
+    println(uniquePairs)
 }
