848c8368b36ce700d5b141707aa5918deac225a6
Ardifa Rizky Saputra <liankylian24@gmail.com>
2026-05-14 22:32:31 +0700

    week11: create string extension functions

---
+++ src/oop_133508_ArdifaRizkySaputra/week11/StringExtensions.kt
@@ -0,0 +1,11 @@
+package oop_133508_ArdifaRizkySaputra.week11
+
+// Extension function dengan receiver 'this'
+fun String.addGreeting(): String {
+    return "Hello, $this"
+}
+
+// Extension function dengan parameter
+fun String.repeatTimes(n: Int): String {
+    return this.repeat(n)
+}
