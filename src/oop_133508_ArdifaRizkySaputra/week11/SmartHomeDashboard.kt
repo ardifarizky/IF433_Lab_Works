edfb6712886fe3e434f23666f456d290637b83e1
Ardifa Rizky Saputra <liankylian24@gmail.com>
2026-05-14 22:35:38 +0700

    week11: (task) configure Philips WiZ using apply

--- src/oop_133508_ArdifaRizkySaputra/week11/SmartHomeDashboard.kt
+++ src/oop_133508_ArdifaRizkySaputra/week11/SmartHomeDashboard.kt
@@ -4,4 +4,12 @@ fun main() {
     println("=== SMART HOME CONFIGURATION PIPELINE ===")
 
     val homeDevices = mutableListOf<SmartDevice>()
+
+    // Konfigurasi Pencahayaan — apply untuk setup, also untuk tambah ke list
+    SmartDevice("Philips WiZ Living Room", "Lighting").apply {
+        isOnline = true
+        powerLoad = 12
+    }.also {
+        homeDevices.add(it)
+    }
 }
